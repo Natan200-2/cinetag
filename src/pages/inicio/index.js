3bffab7b7634617783c6d1b46d57d884106f9e24
Natan200-2 <natanboas21@outlook.com>
2024-01-09 17:24:10 -0300

    titulo de paginas concluido

--- src/pages/inicio/index.js
+++ src/pages/inicio/index.js
@@ -2,12 +2,16 @@ import Cabecalho from 'components/Cabecalho'
 import './Inicio.module.css'
 import Rodape from 'components/Rodape';
 import Banner from 'components/Banner';
+import Titulo from 'components/Titulo';
 
 const Inicio = () => {
     return(
         <>
             <Cabecalho/>
             <Banner image='Home'/>
+            <Titulo>
+                <h1>Um lugar para guardar seus vídeos e filmes!</h1>
+            </Titulo>
             <Rodape />
         </>
     )
