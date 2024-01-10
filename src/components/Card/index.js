afe2113bc7a602068493d2c7688679664543c0b2
Natan200-2 <natanboas21@outlook.com>
2024-01-10 15:04:26 -0300

    Add favoritos concluido

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,12 +1,21 @@
+import { useFavoritoContext } from 'contextos/Favoritos';
 import styles from './Card.module.css'
 import iconeFav from './favoritar.png'
+import iconeDesfav from './desfavoritar.png'
 
 function Card({id, titulo, capa}){
+    const {favorito, adicionarFavorito} = useFavoritoContext();
+    const eFavorito = favorito.some(fav => fav.id === id);
+    const icone = !eFavorito ? iconeFav : iconeDesfav;
+
     return(
         <div className={styles.container}>
             <img src={capa} alt={titulo} className={styles.capa}/>
             <h2>{titulo}</h2>
-            <img src={iconeFav} alt='Favoritar filme' className={styles.favoritar} />
+            <img src={icone} alt='Favoritar filme' className={styles.favoritar} 
+            onClick={ () => {
+                adicionarFavorito({id, titulo, capa})
+            }}/>
         </div>
     )
 }
--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,8 +1,12 @@
 import Banner from 'components/Banner';
 import styles from './Favoritos.module.css';
 import Titulo from 'components/Titulo';
+import { useFavoritoContext } from 'contextos/Favoritos';
+import Card from 'components/Card';
 
 function Favoritos() {
+    const { favorito } = useFavoritoContext();
+
     return(
         <>
             <Banner image='Favoritos'/>
@@ -10,7 +14,7 @@ function Favoritos() {
                 <h1>Meus Favoritos</h1>
             </Titulo>
             <section className={styles.container}>
-
+                {favorito.map(fav => <Card {...fav} key={fav.id}/>)}
             </section>
         </>
     )
